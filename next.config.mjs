d3a62c91633db0e4aadfa6408a1446feeb32e028
Clarence Jordan <clarencejordan087@gamil.com.com>
2025-07-13 21:14:37 -0400

    add es lit ignore

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,18 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   experimental: {
     turbo: {
       rules: {
